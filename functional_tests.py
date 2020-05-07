d7da09d661500f8709199e29d74d344e9f8c9eb9
Calix Huang <calix.huang1@gmail.com>
2020-05-07 11:03:36 -0700

    added chromedriver in gitignore

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome(executable_path='/Users/calixhuang/Documents/chromedriver')
+browser = webdriver.Chrome(executable_path='../obeythetestinggoat/chromedriver')
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
