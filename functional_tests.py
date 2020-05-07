9c492bf11623cbae38c904f8aec7f2fd0c78118d
Calix Huang <calix.huang1@gmail.com>
2020-05-07 11:15:01 -0700

    addded unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(executable_path='../obeythetestinggoat/chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorCase(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path='../obeythetestinggoat/chromedriver')
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_list_and_retrieve_later(self):
+        # Start the browser
+        self.browser.get('http://localhost:8000')
+
+        # Visitor checks the title to see the page
+        self.assertIn("To-Do", self.browser.title)
+
+        # Throw error and stop test
+        self.fail("Finish the e2e test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
