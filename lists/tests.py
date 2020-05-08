19515c0768d9592f29767dc3831d1b1c2059f78b
Calix Huang <calix.huang1@gmail.com>
2020-05-08 15:38:01 -0700

    added item model and wrote unit test for it

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists import views
+from lists import views, models
 
 class HomePageTest(TestCase):
 
@@ -46,3 +46,29 @@ class HomePageTest(TestCase):
 
         # Make sure index.html template is rendered
         self.assertTemplateUsed(response, "index.html")
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        """
+        Test creating and saving the Item model into the database
+        """
+
+        # Create 2 Item objects
+        models.Item.objects.create(text="First List Item")
+        models.Item.objects.create(text="Second List Item")
+
+        # Get all Item objects from database
+        saved_items = models.Item.objects.all()
+
+        # Test how many items are saved in database
+        self.assertEqual(saved_items.count(), 2)
+
+        # Grab each Item object
+        first_item_object = saved_items[0]
+        second_item_object = saved_items[1]
+
+        # Test the 'text' field of each item
+        self.assertEqual(first_item_object.text, "First List Item")
+        self.assertEqual(second_item_object.text, "Second List Item")
