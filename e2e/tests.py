da176cb6bd66f5791f518bad84bf530afd39d8ac
Calix Huang <calix.huang1@gmail.com>
2020-05-11 10:29:08 -0700

    added implicit wait function for table rendering

--- e2e/tests.py
+++ e2e/tests.py
@@ -1,10 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
 class NewVisitorCase(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path='../obeythetestinggoat/chromedriver')
 
@@ -24,14 +26,32 @@ class NewVisitorCase(LiveServerTestCase):
         # Creating new to-do item
         to_do_item_input.send_keys(Keys.ENTER)
 
-        time.sleep(1)
+        # Implicit wait and check for item to be rendered in table
+        self.wait_for_item_rendered_in_table("Buy some milk")
 
-        # Making sure table is populated with new to-do item
-        to_do_item_table = self.browser.find_element_by_id("to-do-item-table")
-        to_do_item_table_rows = to_do_item_table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "Buy some milk" for row in to_do_item_table_rows)
-        )
+
+    def wait_for_item_rendered_in_table(self, item_text):
+        """
+        Implicit wait function until item is rendered into table
+        """
+
+        start_time = time.time()
+        while True:
+            try:
+                # Making sure table is populated with new to-do item
+                to_do_item_table = self.browser.find_element_by_id("to-do-item-table")
+                to_do_item_table_rows = to_do_item_table.find_elements_by_tag_name("tr")
+                self.assertTrue(
+                    any(row.text == "Buy some milk" for row in to_do_item_table_rows)
+                )
+
+                return # End this function
+
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > 10:
+                    raise e
+
+                time.sleep(0.5)
 
 
 if __name__ == "__main__":
