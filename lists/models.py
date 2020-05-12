654181f8fd851a6bcdb27d8ca2ac1d827e622101
Calix Huang <calix.huang1@gmail.com>
2020-05-12 11:33:25 -0700

    added list model - ran into not null db error

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+class List(models.Model):
+    url_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
     text = models.CharField(max_length=50)
+    list = models.ForeignKey("List", on_delete=models.CASCADE, default="")
 
     def __str__(self):
         return self.text
