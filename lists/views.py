9768f7210f05727ea764f38f54a1193a334cc854
Calix Huang <calix.huang1@gmail.com>
2020-05-14 11:56:49 -0700

    refactored views

--- lists/views.py
+++ lists/views.py
@@ -3,15 +3,28 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from . import models
 
 def index(request):
+    """
+    Render the index page
+    """
+
     return render(request, "index.html")
 
 def new_list(request):
-    item_list_name = request.POST["item-list-name"]
-    url_list_name = item_list_name.lower().replace(" ", "-")
-    models.ItemList.objects.create(url_name=url_list_name, name=item_list_name)
-    return HttpResponseRedirect(f"/lists/{url_list_name}/")
+    """
+    Create a new ItemList, then redirect to its unique URL
+    """
+
+    if request.method == "POST":
+        item_list_name = request.POST.get("item-list-name")
+        url_list_name = item_list_name.lower().replace(" ", "-")
+        models.ItemList.objects.create(url_name=url_list_name, name=item_list_name)
+        return HttpResponseRedirect(f"/lists/{url_list_name}/")
 
 def add_item(request):
+    """
+    Create new item and link it to specific ItemList, then redirect back to ItemList page
+    """
+
     if request.method == "POST":
         models.Item.objects.create(
             text = request.POST.get("item"),
@@ -21,10 +34,12 @@ def add_item(request):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def view_list(request, list_url_name):
-    # Get list from list_url_name
+    """
+    View unique ItemList through unique dynamic URL extension
+    """
+
     item_list = models.ItemList.objects.get(url_name=list_url_name)
 
-    # Get all items and send them into context for table population
     return render(request, "view-list.html", context={
         "item_list": item_list,
         "items": item_list.items.all()
