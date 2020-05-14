3ee9598ff172a904067d5be2dad012485b3e82ba
Calix Huang <calix.huang1@gmail.com>
2020-05-14 13:12:13 -0700

    wrote e2e test for list creation and item addition

--- e2e/tests.py
+++ e2e/tests.py
@@ -14,10 +14,14 @@ class NewVisitorCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_start_list_and_retrieve_later(self):
+    def test_create_list_and_add_items(self):
+        """
+        Simulate creating a list and adding items to that list from the unique list view
+        """
         # Start the browser
         self.browser.get(self.live_server_url)
 
+        # ADD ITEM
         # Visitor is invited to create to-do item immediately
         item_list_input = self.browser.find_element_by_id("item-list-input")
 
@@ -27,8 +31,25 @@ class NewVisitorCase(LiveServerTestCase):
         # Creating new to-do item
         item_list_input.send_keys(Keys.ENTER)
 
-        # Check that new list at unique URL is generated
+        # Test that new list at unique URL is generated
         self.assertRegex(self.browser.current_url, "/lists/sample-list/")
+
+        # ADD 3 ITEMS
+        items = ["Item 1", "Item 2", "Item 3"]
+        for item in items:
+            # Grab input element
+            to_do_item_input = self.browser.find_element_by_id("to-do-item-input")
+
+            # Send keys to add item
+            to_do_item_input.send_keys(item)
+            to_do_item_input.send_keys(Keys.ENTER)
+
+        # Test that all 3 items are in table
+        to_do_items_table = self.browser.find_element_by_id("to-do-item-table")
+        for item in items:
+            self.assertTrue(
+                any(row.text == item for row in to_do_items_table.find_elements_by_tag_name("tr"))
+            )
 
 
 if __name__ == "__main__":
