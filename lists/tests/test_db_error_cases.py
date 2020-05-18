55f0f61059deffe0570f4302aaa0ca210abf971a
Calix Huang <calix.huang1@gmail.com>
2020-05-18 09:44:06 -0700

    created 2 (not working) empty error cases at the db level

---
+++ lists/tests/test_db_error_cases.py
@@ -0,0 +1,39 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from lists import views, models
+from unittest import skip
+
+
+class DBErrorCasesTest(TestCase):
+
+    def test_create_empty_item_list_object(self):
+        """
+        Try to create an empty ItemList at the database level
+
+        We are trying to induce an error by creating an empty ItemList
+        """
+
+        # Form incorrect (empty) ItemList object
+        empty_list = models.ItemList(url_name="", name="")
+
+        # Test if DB error is raised
+        with self.assertRaises(ValidationError):
+            empty_list.save()
+
+
+    def test_create_empty_item_object(self):
+        """
+        Try to create an empty Item at the database level
+
+        We are trying to induce an error by creating an empty Item
+        """
+
+        # Form valid list
+        valid_list = models.ItemList.objects.create(url_name="sample-list", name="Sample List")
+
+        # Form incorrect (empty) Item object
+        empty_item = models.Item(item_list=valid_list, text="")
+
+        # Test if DB error is raised
+        with self.assertRaises(ValidationError):
+            empty_item.save()
