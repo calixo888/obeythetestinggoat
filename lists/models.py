f490c89051311b1413280be30b1ea9b5920f72ce
Calix Huang <calix.huang1@gmail.com>
2020-05-18 09:57:51 -0700

    added db error test cases for duplicate object creations

--- lists/models.py
+++ lists/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class ItemList(models.Model):
-    url_name = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    url_name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Item(models.Model):
-    text = models.CharField(max_length=50)
+    text = models.CharField(max_length=50, unique=True)
     item_list = models.ForeignKey(ItemList, on_delete=models.CASCADE, related_name="items")
 
     def __str__(self):
--- lists/tests/test_db_error_cases.py
+++ lists/tests/test_db_error_cases.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
 from lists import views, models
 from unittest import skip
@@ -41,4 +42,42 @@ class DBErrorCasesTest(TestCase):
             empty_item.full_clean()
 
 
-    
+    def test_create_duplicate_item_list_object(self):
+        """
+        Try to create a duplicate ItemList at the database level
+
+        We are trying to induce an error by creating a duplicate ItemList
+        """
+
+        # Form valid ItemList object
+        models.ItemList.objects.create(url_name="sample-list", name="Sample List")
+
+        # Create duplicate ItemList object
+        duplicate_item_list = models.ItemList(url_name="sample-list", name="Sample List")
+
+        # Test integrity of duplicate ItemList
+        with self.assertRaises(IntegrityError):
+            duplicate_item_list.save()
+            duplicate_item_list.full_clean()
+
+
+    def test_create_duplicate_list_object(self):
+        """
+        Try to create a duplicate Item at the database level
+
+        We are trying to induce an error by creating a duplicate Item
+        """
+
+        # Form valid list
+        valid_list = models.ItemList.objects.create(url_name="sample-list", name="Sample List")
+
+        # Form valid Item object
+        models.Item.objects.create(item_list=valid_list, text="Sample List Item")
+
+        # Create duplicate Item object
+        duplicate_item = models.Item(item_list=valid_list, text="Sample List Item")
+
+        # Test integrity of duplicate Item
+        with self.assertRaises(IntegrityError):
+            duplicate_item.save()
+            duplicate_item.full_clean()
